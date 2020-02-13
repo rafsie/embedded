3ba4566d949015dd1bf87e2b7a5d8a03d15c4f6a
RafalS <57662562+rafsie@users.noreply.github.com>
2020-02-13 12:20:45 +0100

    Formatting.

--- ledblink/main.c
+++ ledblink/main.c
@@ -4,36 +4,37 @@
 #include "AT91SAM9263-EK.h"
 #define LED1 (1<<8)
 
-void dbgu_print_ascii(const char* string){
+void dbgu_print_ascii(const char* string)
+{
 }
 
 void del();
 
 *(PIOB_PDR) = 0xFFFFFFFF;
 
-int main(){
-	*AT91C_PIOB_PER = 1<<8;
-	*AT91C_PIOB_OER = 1<<8;
-	unsigned count=0;	
+int main()
+{
+    *AT91C_PIOB_PER = 1<<8;
+    *AT91C_PIOB_OER = 1<<8;
+    unsigned count=0;
 
-	while(1){
-		if(count % 1111 == 0)
-		{
-			*AT91C_PIOB_SODR = LED1;
-			del();
-		}
-		if(count % 2313 == 0)
-		{
-			*AT91C_PIOB_CODR = LED1;
-			del();
-		}
-	++count;
-	}
+    while(1) {
+        if(count % 1111 == 0) {
+            *AT91C_PIOB_SODR = LED1;
+            del();
+        }
+        if(count % 2313 == 0) {
+            *AT91C_PIOB_CODR = LED1;
+            del();
+        }
+        ++count;
+    }
 
-	return 0;
+    return 0;
 }
 
-void del(){
-volatile int i;
-	for(i=0;i<1000000;i++);
+void del()
+{
+    volatile int i;
+    for(i=0; i<1000000; i++);
 }
